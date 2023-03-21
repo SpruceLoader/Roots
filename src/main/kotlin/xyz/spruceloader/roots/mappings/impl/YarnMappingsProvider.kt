f73588775caa4e22e026292dc80c630e17100ff2
TheKodeToad <TheKodeToad@proton.me>
2023-03-21 15:47:28 +0000

    Read the actual zip :P

--- src/main/kotlin/xyz/spruceloader/roots/mappings/impl/YarnMappingsProvider.kt
+++ src/main/kotlin/xyz/spruceloader/roots/mappings/impl/YarnMappingsProvider.kt
@@ -6,6 +6,7 @@ import net.fabricmc.mappingio.tree.MemoryMappingTree
 import xyz.spruceloader.roots.mappings.MappingsProvider
 import java.io.InputStreamReader
 import java.net.URL
+import java.util.zip.ZipInputStream
 
 class YarnMappingsProvider(private val gameVersion: String, private val build: Int) : MappingsProvider {
 
@@ -13,8 +14,10 @@ class YarnMappingsProvider(private val gameVersion: String, private val build: I
         val version = "$gameVersion%2Bbuild.$build"
         val url = URL("https://maven.fabricmc.net/net/fabricmc/yarn/$version/yarn-$version-v2.jar")
         val result = MemoryMappingTree()
-        InputStreamReader(url.openStream()).use {
-            MappingReader.read(it, result)
+        ZipInputStream(url.openStream()).use { zip ->
+            generateSequence(zip::getNextEntry).first { it.name == "mappings/mappings.tiny" }.also {
+                MappingReader.read(InputStreamReader(zip), result)
+            }
         }
         return result
     }
