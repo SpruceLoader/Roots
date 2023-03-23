2ce0d89ee389b070bcfd27408be43b6fbac5a51d
TheKodeToad <TheKodeToad@proton.me>
2023-03-23 18:21:55 +0000

    Use properties in extension

--- src/main/kotlin/xyz/spruceloader/roots/extension/RootsExtension.kt
+++ src/main/kotlin/xyz/spruceloader/roots/extension/RootsExtension.kt
@@ -1,13 +1,18 @@
 package xyz.spruceloader.roots.extension
 
+import org.gradle.api.provider.Property
 import xyz.spruceloader.roots.mappings.provider.IntermediateMappingsProvider
 import xyz.spruceloader.roots.mappings.provider.MappingsProvider
 
 interface RootsExtension {
 
+    fun minecraft(): Property<String>
     fun minecraft(version: String)
 
+    fun intermediate(): Property<IntermediateMappingsProvider>
     fun intermediate(mappings: IntermediateMappingsProvider)
+
+    fun mappings(): Property<MappingsProvider>
     fun mappings(mappings: MappingsProvider)
 
 }
--- src/main/kotlin/xyz/spruceloader/roots/extension/impl/RootsExtensionImpl.kt
+++ src/main/kotlin/xyz/spruceloader/roots/extension/impl/RootsExtensionImpl.kt
@@ -1,25 +1,27 @@
 package xyz.spruceloader.roots.extension.impl
 
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
 import xyz.spruceloader.roots.extension.RootsExtension
 import xyz.spruceloader.roots.mappings.provider.IntermediateMappingsProvider
 import xyz.spruceloader.roots.mappings.provider.MappingsProvider
 
-open class RootsExtensionImpl : RootsExtension {
+open class RootsExtensionImpl(
+    objectFactory: ObjectFactory,
+    private val minecraft: Property<String> = objectFactory.property(String::class.java),
+    private val intermediate: Property<IntermediateMappingsProvider> = objectFactory.property(
+        IntermediateMappingsProvider::class.java
+    ),
+    private val mappings: Property<MappingsProvider> = objectFactory.property(MappingsProvider::class.java)
+) : RootsExtension {
 
-    private var minecraft: String? = null
-    private var intermediate: IntermediateMappingsProvider? = null
-    private var mappings: MappingsProvider? = null
+    override fun minecraft(): Property<String> = minecraft
+    override fun minecraft(version: String) = minecraft.set(version)
 
-    override fun minecraft(version: String) {
-        minecraft = version
-    }
+    override fun intermediate(): Property<IntermediateMappingsProvider> = intermediate
+    override fun intermediate(mappings: IntermediateMappingsProvider) = intermediate.set(mappings)
 
-    override fun intermediate(mappings: IntermediateMappingsProvider) {
-        intermediate = mappings
-    }
-
-    override fun mappings(mappings: MappingsProvider) {
-        this.mappings = mappings
-    }
+    override fun mappings(): Property<MappingsProvider> = mappings
+    override fun mappings(mappings: MappingsProvider) = this.mappings.set(mappings)
 
 }
